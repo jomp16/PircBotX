9120160268b6681d9846e2cdd5075ce99dee3c98
jomp16 <joseoliviopedrosa@gmail.com>
2013-05-01 00:01:45 -0300

    updates

--- src/com/jomp16/pircbotx/sqlite/SQLiteManager.java
+++ src/com/jomp16/pircbotx/sqlite/SQLiteManager.java
@@ -1,7 +1,7 @@
 /*
  * Copyright (C) 2013 jomp16
  *
- * This file is part of PircBotX.
+ * This file is part of PircBotX
  *
  * PircBotX is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -10,11 +10,11 @@
  *
  * PircBotX is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with PircBotX.  If not, see <http://www.gnu.org/licenses/>.
+ * along with PircBotX. If not, see <http://www.gnu.org/licenses/>.
  */
 
 package com.jomp16.pircbotx.sqlite;
